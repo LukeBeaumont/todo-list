8c2a77896dcb6d50cb6c74f8a58d870b3855cf34
LukeBeaumont <lbeaumont50@msn.com>
2023-11-12 15:54:17 +0000

    add event listners to add buttons

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import "./styles.css";
 import CreateProject from "./create-project";
 import CreateTodo from "./create-todo";
+
+(function showAddProject() {
+  const addProjectBtn = document.querySelector(".add-project-btn");
+  const addProject = document.querySelector(".add-project");
+
+  addProjectBtn.addEventListener("click", () => {
+    addProject.style.display = "flex";
+  });
+})();
+
+(function showAddTodo() {
+  const addTodoBtn = document.querySelector(".add-todo-btn");
+  const addTodo = document.querySelector(".add-todo");
+
+  addTodoBtn.addEventListener("click", () => {
+    addTodo.style.display = "flex";
+  });
+})();
